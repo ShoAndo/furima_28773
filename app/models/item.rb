f0dc844b86592e3ab30a712850879e5be8b073eb
Sho Ando <spdtas0223@gmail.com>
2020-08-01 16:46:07 +0900

    サンプルの出品が表示されるように設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  extend Activehash::Associations::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :item_status
   belongs_to_active_hash :delivery_burden
@@ -8,8 +8,8 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates name, length: { maxmum: 40 }
-    validates text, length: { maxmum: 1000 }
+    validates :name, length: { maximum: 40 }
+    validates :text, length: { maximum: 1000 }
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
                                       message: 'is out of setting range' }
   end
--- app/models/send_date.rb
+++ app/models/send_date.rb
@@ -1,4 +1,4 @@
-class SendDate < Activehash::Base
+class SendDate < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '1~2日で発送' },
