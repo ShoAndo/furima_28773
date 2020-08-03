250d2bc5f669af0c59741cedf0d43a57b1241c43
Sho Ando <spdtas0223@gmail.com>
2020-08-03 19:35:41 +0900

    itemの単体テストのダミーデータの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::Name.initials }
-    text { Faker::Name.name_with_middle }
-    price { Faker::Number.between(from: 300, to: 9_999_999) }
-    category_id { Faker::Number.between(from: 2, to: 11 ) }
-    item_status_id { Faker::Number.between(from: 2, to: 7) }
-    delivery_burden_id { Faker::Number.between(from: 2, to: 3) }
-    prefecture_id { Faker::Number.between(from: 2, to: 48) }
-    send_date_id { Faker::Number.between(from: 2, to: 4) }
+    name { 'サンプルデータ' }
+    text { 'サンプルサンプルサンプルサンプル' }
+    price { 1000 }
+    category_id { 3 }
+    item_status_id { 3 }
+    delivery_burden_id { 2 }
+    prefecture_id { 14 }
+    send_date_id { 3 }
     association :user
 
   end
