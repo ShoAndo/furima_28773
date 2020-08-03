7f40065a1f31905da4870d02ec5a965660d8b66b
Sho Ando <spdtas0223@gmail.com>
2020-08-03 19:20:33 +0900

    出品機能の単体テスト実装　sent_area.rbは不要なファイルです。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,14 @@
 FactoryBot.define do
   factory :item do
+    name { Faker::Name.initials }
+    text { Faker::Name.name_with_middle }
+    price { Faker::Number.between(from: 300, to: 9_999_999) }
+    category_id { Faker::Number.between(from: 2, to: 11 ) }
+    item_status_id { Faker::Number.between(from: 2, to: 7) }
+    delivery_burden_id { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    send_date_id { Faker::Number.between(from: 2, to: 4) }
+    association :user
+
   end
 end
