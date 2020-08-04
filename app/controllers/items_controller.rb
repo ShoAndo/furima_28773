41186373dc0b856b4d24e11f27cea72904e6972f
Sho Ando <spdtas0223@gmail.com>
2020-08-04 12:01:03 +0900

    削除機能実装(新規登録時のエラーメッセージを出力できなかったので、その点も今回のブランチとは違いますが、修正しています)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :find_item, only: :show
+  before_action :find_item, only: [:show, :destroy]
 
   def index
     @items = Item.all
@@ -26,6 +26,11 @@ class ItemsController < ApplicationController
     @purchases = Purchase.all
   end
 
+  def destroy
+    @item.destroy
+    redirect_to action: :index
+  end
+
   private
 
   def item_params
@@ -35,6 +40,7 @@ class ItemsController < ApplicationController
 
   def find_item
     @item = Item.find(params[:id])
+  end
 
   def move_to_index
     unless user_signed_in?
