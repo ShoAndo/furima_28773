d45513ba0a1d0e569daecfe9fa4dd503978d1f06
Sho Ando <spdtas0223@gmail.com>
2020-08-04 22:05:24 +0900

    コメントいただいたところの修正

--- app/forms/user_pay.rb
+++ app/forms/user_pay.rb
@@ -3,11 +3,11 @@ class UserPay
   include ActiveModel::Model
   attr_accessor :price, :post_number, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_id
 
-  
+  post_num = "\A\d{3}[-]\d{4}\z"
   phone_num = "[0-9]{,11}"
   with_options presence: true do
     validates :prefecture_id, :city, :address
-    validates :post_number, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :post_number, format: { with: /#{post_num}/, message: "is invalid. Include hyphen(-)" }
     validates :phone_number, format: { with: /#{phone_num}/, message: "is invalid. Max length is 11 numbers" }
     validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: "is out of setting range"}
   end
