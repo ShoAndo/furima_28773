03105bae9a90ed92b2045ecc4c415955972c08f5
Sho Ando <spdtas0223@gmail.com>
2020-08-04 20:39:41 +0900

    購入機能実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,46 @@
+class OrdersController < ApplicationController
+  before_action :move_to_index, :find_item
+
+  def index
+    authenticate_user!
+    @user_pay = UserPay.new
+  end
+
+  def create
+    @user_pay = UserPay.new(order_params)
+   binding.pry
+   if @user_pay.valid?
+     pay_item
+     @user_pay.save
+     return redirect_to root_path
+   else
+     render :index
+   end
+  end
+
+  private
+
+  def order_params
+    params.require(:user_pay).permit(:post_number, :prefecture_id, :city, :address,
+                                     :building, :phone_number).merge(price: @item.price, user_id: current_user.id, item_id: @item.id)
+  end
+ 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],
+      card: params[:token],
+      currency:'jpy'
+    )
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+end
---
+++ app/forms/user_pay.rb
@@ -0,0 +1,23 @@
+class UserPay
+
+  include ActiveModel::Model
+  attr_accessor :price, :post_number, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_id
+
+  
+  phone_num = "[0-9]{,11}"
+  with_options presence: true do
+    validates :prefecture_id, :city, :address
+    validates :post_number, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :phone_number, format: { with: /#{phone_num}/, message: "is invalid. Max length is 11 numbers" }
+    validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: "is out of setting range"}
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1 }
+
+  def save
+    Order.create(price: price)
+    Purchase.create(item_id: item_id, user_id: user_id)
+    Address.create(post_number: post_number, prefecture_id: prefecture_id, city: city, address: address, 
+                   building: building, phone_number: phone_number, item_id: item_id)
+  end
+end
