a838e974cf422f3432f809ea18e83231a4987eaa
Sho Ando <spdtas0223@gmail.com>
2020-08-10 12:07:27 +0900

    プレビュー画像を見れるようにする

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,19 +1,22 @@
-function calc(){
-  const input = document.getElementById('item_price')
-  const add_tax_price = document.getElementById('add-tax-price')
-  const profit = document.getElementById('profit')
+if (window.location.pathname === '/items/new' || window.location.pathname ==='items/edit') {
 
-  input.addEventListener( 'input', (e) =>{
-    const input_price = document.getElementById('item_price').value;
-    if (input_price == 0){
-      add_tax_price.innerHTML = '半角数字のみ入力可能'
-      profit.innerHTML = '半角数字のみ入力可能'
-    } else {
-      add_tax_price.innerHTML = Math.floor(input_price * 0.1)
-      profit.innerHTML = Math.floor(input_price * 0.9)
-    }
-    e.preventDefault()
-  });
+  function calc(){
+    const input = document.getElementById('item_price')
+    const add_tax_price = document.getElementById('add-tax-price')
+    const profit = document.getElementById('profit')
+
+    input.addEventListener( 'input', (e) =>{
+      const input_price = document.getElementById('item_price').value;
+      if (input_price == 0){
+        add_tax_price.innerHTML = '半角数字のみ入力可能'
+        profit.innerHTML = '半角数字のみ入力可能'
+      } else {
+        add_tax_price.innerHTML = Math.floor(input_price * 0.1)
+        profit.innerHTML = Math.floor(input_price * 0.9)
+      }
+      e.preventDefault()
+    });
+  }
+
+  setInterval(calc, 1000)
 }
-
-setInterval(calc, 1000)
